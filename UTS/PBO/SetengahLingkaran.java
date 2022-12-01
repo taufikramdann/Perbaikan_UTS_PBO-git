b81f79b44d71d1389593a884da65101c538d1ab3
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-12-01 20:06:54 +0700

    Create SetengahLingkaran.java

---
+++ UTS/PBO/SetengahLingkaran.java
@@ -0,0 +1,21 @@
+paket  UT . com ;
+
+public  class  SetengahLingkaran  mengimplementasikan  BangunDatar {
+     jari2 ganda ;
+    double  luasSetengahLingkaran ;
+
+    publik  SetengahLingkaran ( diameter ganda  ) {
+        ini . jari2 = diameter / 2 ;
+    }
+
+    @ Menimpa
+    public  void  luas () {
+        luasSetengahLingkaran = jari2 * jari2 * 3,14 * 0,5 ;
+    }
+
+    @ Menimpa
+     getLuas ganda  publik () {
+        return  luasSetengahLingkaran ;
+    }
+
+}
