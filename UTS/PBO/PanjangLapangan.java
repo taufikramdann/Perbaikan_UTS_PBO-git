5a7b0227e45b68fa3d92af0cae118a5f716e14d7
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-12-01 20:14:58 +0700

    Create PanjangLapangan.java

---
+++ UTS/PBO/PanjangLapangan.java
@@ -0,0 +1,23 @@
+paket  UT . com ;
+
+public  class  JalanPanjang  mengimplementasikan  BangunDatar {
+     panjang ganda ;
+     lebar ganda ;
+    ganda  luasPersegiPanjang ;
+
+    public  JalanPanjang ( double  panjang , double  lebar ) {
+        ini . panjang = panjang ;
+        ini . lebar = lebar ;
+    }
+
+    @ Menimpa
+    public  void  luas () {
+        luasPersegiPanjang = ini . panjang * ini . lebar ;
+    }
+
+    @ Menimpa
+     getLuas ganda  publik () {
+        kembalikan  luasPersegiPanjang ;
+    }
+
+}
